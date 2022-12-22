2891b78c828acbc69b8afe6ece8c0b87eb26d853
Ron Morgenstern <ron.morgenstern@pagaya.com>
2022-12-22 10:07:11 +0200

    commit

--- setup.sh
+++ setup.sh
@@ -7,11 +7,14 @@ sudo rm -rf ~/.tmux.conf >/dev/null 2>&1
 sudo rm -rf ~/.zshrc >/dev/null 2>&1
 sudo rm -rf ~/.gitconfig >/dev/null 2>&1
 sudo rm -rf ~/.gitignore >/dev/null 2>&1
+sudo rm -rf ~/.config/alacritty >/dev/null 2>&1
+sudo rm -rf ~/.config/bat >/dev/null 2>&1
 
 #==============
 # Create symlinks in the home folder
 # Allow overriding with files of matching names in the custom-configs dir
 #==============
+
 SYMLINKS=()
 ln -sf ~/dotfiles/vim ~/.vim
 SYMLINKS+=('.vim')
@@ -25,5 +28,9 @@ ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
 SYMLINKS+=('.gitconfig')
 ln -sf ~/dotfiles/git/.gitignore ~/.gitignore
 SYMLINKS+=('.gitignore')
+ln -sf ~/dotfiles/config/alacritty ~/.config/alacritty
+SYMLINKS+=('alacritty')
+ln -sf ~/dotfiles/config/bat ~/.config/bat
+SYMLINKS+=('bat')
 
 echo ${SYMLINKS[@]}
