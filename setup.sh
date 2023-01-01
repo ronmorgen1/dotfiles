e2fcdf2a8941cd4ce7121d4cf28b5588b09f09d4
Ron Morgenstern <ron.morgenstern@pagaya.com>
2023-01-01 16:02:30 +0200

    update

--- setup.sh
+++ setup.sh
@@ -2,24 +2,15 @@
 
 set -e
 
-declare -a dotfiles=(
-  ".zshrc" 
-  ".vimrc" 
-  ".gitignore" 
-  ".gitconfig" 
-  ".tmux.conf" 
-  ".config/alacritty" 
-  ".config/bat" 
-  ".vim"
-)
+# files
+ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
+ln -sf "$PWD/.vimrc" "$HOME/.vimrc"
+ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$PWD/git/.gitignore" "$HOME/.gitignore"
+ln -sf "$PWD/git/.gitconfig" "$HOME/.gitconfig"
 
-for item in "${dotfiles[@]}"; do
-  # Check if the file already exists in the home directory
-  if [ -e "$HOME/$item" ]; then
-    printf 'Removing existing file %s\n' "$item"
-    rm -rf "$HOME/$item" >/dev/null 2>&1
-  fi
-  # Create a symlink to the file in the current directory
-  printf 'Linking %s\n\n' "$item"
-  ln -sf "$PWD/$item" "$HOME/$item"
-done
+# Directories
+ln -sf "$PWD/zsh" "$HOME/zsh"
+ln -sf "$PWD/vim" "$HOME/.vim"
+ln -sf "$PWD/config/alacritty" "$HOME/.config/alacritty"
+ln -sf "$PWD/config/bat" "$HOME/.config/bat"
